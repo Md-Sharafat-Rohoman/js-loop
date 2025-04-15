631079ca67807c41ae15db23fc7648c781c98fd7
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2025-04-16 01:11:00 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,5 @@
+const numbers = [12,32,33,43,23,45,66];
+// console.log(numbers);
+for(const number of numbers){
+    console.log(number);
+}
---
+++ sum.js
@@ -0,0 +1,13 @@
+let num = 1;
+let sum = 0;
+while (num <= 10) {
+    // console.log(num);
+    if (num % 2 == 1) {
+        sum = sum + num;
+
+    }
+    // console.log(sum)
+    num++;
+
+}
+console.log(sum)
