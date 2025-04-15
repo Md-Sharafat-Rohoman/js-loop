0926a7dd1d1c5f63319277a7e08f6b6f94fe0cff
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2025-04-16 01:52:19 +0600

    continue commit

--- different-for.js
+++ different-for.js
@@ -6,4 +6,16 @@ for(let i=31; i<=45; i++){
         sum = sum + i;
     }
 }
-console.log(sum)
+console.log(sum);
+
+
+
+let total = 0;
+for(let i=1; i<=50; i++){
+    // console.log(i)
+    if(i%5 === 0){
+        // console.log(i)
+        total = total + i;
+    }
+}
+console.log(total)
